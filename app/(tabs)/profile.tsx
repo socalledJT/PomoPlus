22e79e1d1ad0c61ee99ffd8f012c8fb7514bd686
Saimir Brahja <36928647+socalledJT@users.noreply.github.com>
2024-11-30 16:27:25 +0100

    Minor update of Profile page
    ~ Finished one card on the profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,8 @@
-import { Image, StyleSheet, Platform, View, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, Platform, View, TouchableOpacity, Text } from 'react-native';
 import router, { useRouter } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient'
-import { HelloWave } from '@/components/HelloWave';
+
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -24,12 +24,20 @@ export default function HomeScreen() {
       <ThemedView style={AreaContainers.buttonContainer}>
         <LinearGradient
         // Background Linear Gradient
-        colors={['#2400FF', '#2D2E4D']}
+        colors={['rgba(45, 46, 77, 1)', 'rgba(36, 0, 255, 0.7)']}
         start={[0, 0.5]}
         end={[0.5, 1]}
         style={AreaContainers.profilecontainer}
         >
-          <text>Test</text>
+          <View>
+            <Image style={styles.profileImage}></Image> {/*source={require('./assets/images/Placeholder.png')} />*/}
+          </View>
+          <View>
+            <Text style={styles.profileNameText}>JohnnyTm</Text>
+          </View>
+          <View>
+            <Text style={styles.profileLevelText}>Level: 69</Text>
+          </View>
         </LinearGradient>
       </ThemedView>
     </ThemedSafeAreaView>
@@ -44,5 +52,23 @@ const styles = StyleSheet.create({
     // align the icon of the button in the center
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  profileNameText: {
+    color: '#fff'
+  },
+  profileLevelText: {
+    color: '#C4C4C4',
+    fontSize: 10,
+
+    marginTop: 3
+  },
+  profileImage: {
+    backgroundColor: "black",
+    width: 100,
+    height: 100,
+
+    borderRadius: 100,
+    marginTop: -60,
+    marginBottom: 15
   }
 });
